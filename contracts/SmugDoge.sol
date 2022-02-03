78bf6a51221edef6d4638a0e432e784e40c8da2b
hunter <hunter0409>
2022-02-03 09:35:57 +0300

    SmugDogeSmartContract

---
+++ contracts/SmugDoge.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract SmugDoge is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    uint256 private _maxSupply = 690;
+    string public _baseURL;
+    uint256 public launchDate;
+    mapping(uint256 => string) tokenUrls;
+
+    constructor() ERC721("SmugDoge", "SMD") {
+        setBaseURL("ipfs://");
+    }
+
+    function mint(string memory metadataURI) external payable {
+        require(
+            _tokenIds.current() < _maxSupply,
+            "Can not mint more than max supply"
+        );
+        require(msg.value >= 420 ether, "Insufficient payment");
+
+        _safeMint(msg.sender, _tokenIds.current());
+        // _setTokenURI(_tokenIds.current(), metadataURI);
+        _tokenIds.increment();
+    }
+
+    function getTokenURL(uint256 tokenId) public view returns (string memory) {
+        return tokenUrls[tokenId];
+    }
+
+    function setBaseURL(string memory baseURI) public onlyOwner {
+        _baseURL = baseURI;
+    }
+
+    function setMaxSupply(uint256 value) public onlyOwner {
+        _maxSupply = value;
+    }
+
+    // function _baseURI() internal view returns (string memory) {
+    //     return _baseURL;
+    // }
+
+    function maxSupply() public view returns (uint256) {
+        return _maxSupply;
+    }
+
+    // function totalSupply() public view override returns (uint256) {
+    //     return _tokenIds.current();
+    // }
+
+    function withdrawONE(address payable _addr, uint256 amount)
+        public
+        onlyOwner
+    {
+        _addr.transfer(amount);
+    }
+
+    // function withdrawONEOwner(uint256 amount) public onlyOwner {
+    //     msg.sender.transfer(amount);
+    // }
+}
